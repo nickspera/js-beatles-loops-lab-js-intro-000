ced97d18ab00f0bc74e55673279139a03c565059
Nick Spera <nickaspera@gmail.com>
2019-08-27 02:02:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 function theBeatlesPlay(musicians, instruments) {
         const arr = []
 
-  for (let i = 0; i < l; l = musicians.length; i ++){
+  for (let i = 0; l = musicians.length; i < l; i++){
     arr.push(`${musicians[i]} plays ${instruments[i]}`)
   }
+  
   return arr
 }
